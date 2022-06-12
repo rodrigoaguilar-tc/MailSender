26533dc3c14bd8ee359f6f9545750b2e5fa1be8d
Rodrigo Aguilar <rodrigoaguilar96@gmail.com>
2022-06-12 17:24:01 -0300

    Add validations for requestDTO

--- src/main/java/com/aguilar/mailsender/controller/MailController.java
+++ src/main/java/com/aguilar/mailsender/controller/MailController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/sendMail")
 public class MailController {
@@ -20,7 +22,7 @@ public class MailController {
   MailService mailService;
 
   @PostMapping("/simpleMail")
-  public ResponseEntity<SimpleMsgResponseDTO> sendMail(@RequestBody SimpleMsgRequestDTO simpleMsgRequestDTO) {
+  public ResponseEntity<SimpleMsgResponseDTO> sendMail(@Valid @RequestBody SimpleMsgRequestDTO simpleMsgRequestDTO) {
     return new ResponseEntity<>(mailService.sendSimpleMessage(simpleMsgRequestDTO), HttpStatus.OK);
   }
 }
--- src/main/java/com/aguilar/mailsender/controller/dto/SimpleMsgRequestDTO.java
+++ src/main/java/com/aguilar/mailsender/controller/dto/SimpleMsgRequestDTO.java
@@ -2,10 +2,17 @@ package com.aguilar.mailsender.controller.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Data
 public class SimpleMsgRequestDTO {
 
+  @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
   private String mailTo;
+
   private String mailsSubject;
+
+  @NotBlank
   private String mailBody;
 }
