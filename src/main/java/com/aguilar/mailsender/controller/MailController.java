567fd2df93ddc5b8e476ac4c3bb8ce9fc8876360
Rodrigo Aguilar <38480439+rodrigoaguilar-tc@users.noreply.github.com>
2022-06-12 16:55:08 -0300

    Create README.md

--- src/main/java/com/aguilar/mailsender/controller/MailController.java
+++ src/main/java/com/aguilar/mailsender/controller/MailController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/sendMail")
-public class mailController {
+public class MailController {
 
   @Autowired
   MailService mailService;
